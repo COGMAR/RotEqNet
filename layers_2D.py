Fixed some minor bugs in filter-rotation.

layers_2D.py
@@ -32,13 +32,6 @@ class RotConv(nn.Module):
 
         self.mode = mode
 
-        self.weight1 = Parameter(torch.Tensor( out_channels, in_channels , *kernel_size))
-
-        #If input is vector field, we have two filters (one for each component)
-        if self.mode == 2:
-            self.weight2 = Parameter(torch.Tensor( out_channels, in_channels, *kernel_size))
-
-
         #Angles
         self.angles = np.linspace(0,360,n_angles, endpoint=False)
         self.angle_tensors = []
@@ -46,8 +39,16 @@ class RotConv(nn.Module):
         #Get interpolation variables
         self.interp_vars = []
         for angle in self.angles:
-            self.interp_vars.append(get_filter_rotation_transforms(self.kernel_size, angle))
+            out = get_filter_rotation_transforms(self.kernel_size, angle)
+            self.interp_vars.append(out[:-1])
+            self.mask = out[-1]
+
             self.angle_tensors.append( Variable(torch.FloatTensor( np.array([angle/ 180. * np.pi]) )) )
+
+        self.weight1 = Parameter(torch.Tensor( out_channels, in_channels , *kernel_size))
+        #If input is vector field, we have two filters (one for each component)
+        if self.mode == 2:
+            self.weight2 = Parameter(torch.Tensor( out_channels, in_channels, *kernel_size))
 
         self.reset_parameters()
 
@@ -60,10 +61,15 @@ class RotConv(nn.Module):
         if self.mode == 2:
             self.weight2.data.uniform_(-stdv, stdv)
 
+    def mask_filters(self):
+        self.weight1 = Parameter(self.weight1.clone().data * self.mask+.00000001)
+        if self.mode == 2:
+            self.weight2 = Parameter(self.weight2.clone().data * self.mask+.00000001)
+
     def _apply(self, func):
         # This is called whenever user calls model.cuda()
         # We intersect to replace tensors and variables with cuda-versions
-
+        self.mask = func(self.mask)
         self.interp_vars = [[[func(el2) for el2 in el1] for el1 in el0] for el0 in self.interp_vars]
         self.angle_tensors = [func(el) for el in self.angle_tensors]
 
@@ -71,6 +77,9 @@ class RotConv(nn.Module):
 
 
     def forward(self,input):
+        #Uncomment this to turn on filter-masking
+        #Todo: fix broken convergence when filter-masking is on
+        #self.mask_filters()
 
         if self.mode == 1:
             outputs = []
@@ -87,7 +96,7 @@ class RotConv(nn.Module):
             #Get the maximum direction (Orientation Pooling)
             strength, max_ind =  torch.max(torch.cat(outputs,-1),-1)
 
-            #Convert to polar format
+            #Convert from polar representation
             angle_map = max_ind.float() * (360. / 8. / 180. * np.pi)
             u = F.relu(strength) * torch.cos(angle_map)
             v = F.relu(strength) * torch.sin(angle_map)
@@ -131,7 +140,7 @@ class RotConv(nn.Module):
 
             u = torch.gather(u, -1, max_ind.unsqueeze(-1))[:,:,:,:,0]
             v = torch.gather(v, -1, max_ind.unsqueeze(-1))[:,:,:,:,0]
-        
+
         return u, v
 
 class VectorMaxPool(nn.Module):
@@ -186,7 +195,7 @@ class Vector2Magnitude(nn.Module):
         return p
 
 class VectorBatchNorm(nn.Module):
-    def __init__(self, num_features, eps=1e-5, momentum=0.5, affine=True):
+    def     __init__(self, num_features, eps=1e-5, momentum=0.5, affine=True):
 
         super(VectorBatchNorm, self).__init__()
         self.num_features = num_features

mnist/mnist_test.py
@@ -51,6 +51,7 @@ if __name__ == '__main__':
                 nn.ReLU(),
                 nn.Dropout2d(0.7),
                 nn.Conv2d(128, 10, 1),  # FC2
+
             )
 
         def forward(self, x):
@@ -60,7 +61,7 @@ if __name__ == '__main__':
             return x
 
 
-    gpu_no =  1 # Set to False for cpu-version
+    gpu_no =  0 # Set to False for cpu-version
 
     #Setup net, loss function, optimizer and hyper parameters
     net = Net()
@@ -92,18 +93,17 @@ if __name__ == '__main__':
         return data.astype('float32')
 
 
-    def test(model, dataset):
+    def test(model, dataset, mode):
         """ Return test-acuracy for a dataset"""
         model.eval()
 
         true = []
         pred = []
         for batch_no in xrange(len(dataset) / batch_size):
-            data, labels = getBatch(dataset, 'test')
-
+            data, labels = getBatch(dataset, mode)
 
             #Run same sample with different orientations through network and average output
-            if use_test_time_augmentation:
+            if use_test_time_augmentation and mode == 'test':
                 data = data.cpu()
                 original_data = data.clone().data.cpu().numpy()
 
@@ -207,6 +207,7 @@ if __name__ == '__main__':
             loss = criterion( out,labels )
             _, c = torch.max(out, 1)
             loss.backward()
+
             optimizer.step()
 
             #Print training-acc
@@ -215,7 +216,7 @@ if __name__ == '__main__':
 
 
         #Validation
-        acc = test(net, val_set[:])
+        acc = test(net, val_set[:], 'val')
         print 'Val',  'epoch:', epoch_no,  ' acc:', acc
 
         #Save model if better than previous
@@ -228,7 +229,7 @@ if __name__ == '__main__':
 
     # Finally test on test-set with the best model
     net.load_state_dict(torch.load('best_model.pt'))
-    print 'Test', 'acc:', test(net, test_set[:])
+    print 'Test', 'acc:', test(net, test_set[:], 'test')
 
 
     

utils.py
@@ -65,17 +65,21 @@ def get_filter_rotation_transforms(kernel_dims, angles):
     #Mask out samples outside circle
     radius = np.expand_dims(radius,-1)
     dist_to_center = np.sqrt(np.sum(grid**2,axis=0))
-    mask = dist_to_center>=radius*10
+    mask = dist_to_center>=radius+.0001
+    mask = 1-mask
 
     # Move grid to center
     grid += radius
 
-    return compute_interpolation_grids(grid, kernel_dims, mask=mask)
+    return compute_interpolation_grids(grid, kernel_dims, mask)
 
-def compute_interpolation_grids(grid, kernel_dims, mask=None):
+def compute_interpolation_grids(grid, kernel_dims, mask):
 
     #######################################################
     # The following part is part of nd-linear interpolation
+
+    #Add a small eps to grid so that floor and ceil operations become more stable
+    grid += 0.000000001
 
     # Make list where each element represents a dimension
     grid = [grid[i, :] for i in range(grid.shape[0])]
@@ -87,46 +91,52 @@ def compute_interpolation_grids(grid, kernel_dims, mask=None):
     # Get weights
     weights = [float_ind - int_ind for float_ind, int_ind in zip(grid, inds_0)]
 
-    # Get samples that are out of bounds
-    inds_out_of_bounds = np.logical_or.reduce([ind < 0 for ind in itertools.chain(inds_0, inds_1)] + \
-                                              [ind >= siz for ind, siz in zip(inds_0, kernel_dims)] + \
-                                              [ind >= siz for ind, siz in zip(inds_1, kernel_dims)])
+    # Special case for when ind_1 == size (while ind_0 == siz)
+    # In that case we select ind_0
+    ind_1_out_of_bounds = np.logical_or.reduce([ind == siz for ind, siz in zip(inds_1, kernel_dims)])
+    for i in range(len(inds_1)):
+        inds_1[i][ind_1_out_of_bounds] = 0
 
-    # Set these samples to zero get data from upper-left-corner
+
+    # Get samples that are out of bounds or outside mask
+    inds_out_of_bounds = np.logical_or.reduce([ind < 0 for ind in itertools.chain(inds_0, inds_1)] + \
+                                              [ind > siz for ind, siz in zip(inds_0, kernel_dims)] + \
+                                              [ind >= siz for ind, siz in zip(inds_1, kernel_dims)] +
+                                              (1-mask).astype('bool')
+                                              )
+
+
+    # Set these samples to zero get data from upper-left-corner (which will be put to zero)
     for i in range(len(inds_0)):
         inds_0[i][inds_out_of_bounds] = 0
         inds_1[i][inds_out_of_bounds] = 0
-
-    #######################################################
-    if mask is not None:
-        zero_inds = np.logical_or(inds_out_of_bounds, mask)
-    else:
-        zero_inds = inds_out_of_bounds
-    zero_inds = np.reshape(zero_inds, kernel_dims)
-
 
     #Make pytorch-tensors of the interpolation variables
     inds_0 = [torch.LongTensor(ind) for ind in inds_0]
     inds_1 = [torch.LongTensor(ind) for ind in inds_1]
     weights = [Variable(torch.FloatTensor(weight)) for weight in weights]
-    zero_inds = [Variable(torch.FloatTensor(zero_inds.astype('float32')))]
+
+    #Make mask pytorch tensor
+    mask = mask.reshape(kernel_dims)
+    mask = mask.astype('float32')
+    mask = np.expand_dims(mask, 0)
+    mask = np.expand_dims(mask, 0)
+    mask = torch.FloatTensor(mask)
 
     # Uncomment for nearest interpolation (for debugging)
     #inds_1 = [ind*0 for ind in inds_1]
     #weights  = [weight*0 for weight in weights]
 
-    return inds_0, inds_1, weights, zero_inds
+    return inds_0, inds_1, weights, mask
 
 def apply_transform(filter, interpolation_variables, filters_size):
     """ Apply a transform specified by the interpolation_variables to a filter """
 
     dim = 2 if len(filter.size())==4 else 3
 
-
     if dim == 2:
 
-        [x0_0, x1_0], [x0_1, x1_1], [w0, w1], zero_inds = interpolation_variables
-        zero_inds = zero_inds[0] #Unpack
+        [x0_0, x1_0], [x0_1, x1_1], [w0, w1] = interpolation_variables
 
         rotated_filter = (filter[:, :, x0_0, x1_0] * (1 - w0) * (1 - w1) +
                           filter[:, :, x0_1, x1_0] * w0 * (1 - w1) +
@@ -135,11 +145,8 @@ def apply_transform(filter, interpolation_variables, filters_size):
 
         rotated_filter = rotated_filter.view(filter.size()[0],filter.size()[1],filters_size[0],filters_size[1])
 
-
-
     elif dim == 3:
-        [x0_0, x1_0, x2_0], [x0_1, x1_1, x2_1], [w0, w1, w2], zero_inds = interpolation_variables
-        zero_inds = zero_inds[0]  # Unpack
+        [x0_0, x1_0, x2_0], [x0_1, x1_1, x2_1], [w0, w1, w2] = interpolation_variables
 
         rotated_filter = (filter[x0_0, x1_0, x2_0] * (1 - w0) * (1 - w1)* (1 - w2) +
                           filter[x0_1, x1_0, x2_0] * w0       * (1 - w1)* (1 - w2) +
@@ -152,8 +159,6 @@ def apply_transform(filter, interpolation_variables, filters_size):
 
         rotated_filter = rotated_filter.view(filter.size()[0], filter.size()[1], filters_size[0], filters_size[1], filters_size[2])
 
-    #Apply mask to zero-pixels that are outside valid region
-    rotated_filter = rotated_filter * (1 - zero_inds)
     return rotated_filter
 
 
@@ -170,13 +175,15 @@ if __name__ == '__main__':
     angle = 45
     interp_vars = get_filter_rotation_transforms(ks, angle)
 
-    w = Variable(torch.zeros([1,1]+ks))
+    w = Variable(torch.ones([1,1]+ks))
     w[:,:,4,:] = 5
-    w[:, :, :, 4] = 1
-    w[:,:,0,0] = -1
+    w[:, :, :, 4] = 5
+    #w[:,:,0,0] = -1
 
 
-
-    print w, apply_transform(w, interp_vars, ks)
+    print w
+    for angle in [0,90,45,180,65,10]:
+        print angle,'degrees'
+        print apply_transform(w, get_filter_rotation_transforms(ks, angle)[:-1], ks) * Variable(get_filter_rotation_transforms(ks, angle)[-1])
 
 